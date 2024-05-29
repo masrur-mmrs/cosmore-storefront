e9f83625feb0a5b4a74fd95b35e270ecc95aaec8
masrur-mmrs <masrur.mmrs@gmail.com>
2024-05-29 14:02:20 +0600

    change acordian color

--- src/app/[countryCode]/(main)/loading.tsx
+++ src/app/[countryCode]/(main)/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading() {
-    return (<div className="h-[100vh] w-[100vw] flex items-center justify-items-center justify-center overflow-x-hidden">
+    return (<div className="h-[100vh] w-[98vw] flex items-center justify-items-center justify-center overflow-x-hidden">
                 <span className="loader"></span>
             </div>)
   }
